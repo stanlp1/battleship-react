e02b9395b6f571f8571ea77f97e969a1ea9cb5e0
stanlp1 <stanleyp101@gmail.com>
2021-10-14 20:56:19 -0700

    added socket server, finished room joining mechanism

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 4000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({server});
+
+const MAX_PLAYERS = 2;
+let rooms = new Map();
+
+let getUniqueID = () => {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+}
+
+let userEvents = {
+    JOIN_ROOM: 0,
+    PLACE_SHIP: 1,
+    MAKE_MOVE: 2,
+    GAME_OVER: 3,
+}
+
+let sendToClient = (clientId, data) =>
+{
+    wss.clients.forEach((ws) => {
+        if (ws.id === clientId){
+            if (typeof data === "string")
+                ws.send(data);
+            else
+                ws.send(JSON.stringify(data));
+        }
+    })
+}
+wss.on('connection', function connection(ws) {
+    ws.id = getUniqueID();
+    ws.on('message', function incoming(data) {
+        let jsonData = JSON.parse(data);
+        let event = jsonData.event;
+        let payload = jsonData.payload;
+        console.log(jsonData);
+        switch (event)
+        {
+            case userEvents.JOIN_ROOM:
+            {
+                let room = rooms.get(payload.ROOM_NAME);
+                if (room !== undefined)
+                {
+                    if (room.curPlayers < MAX_PLAYERS)
+                    {
+                        room.curPlayers++;
+                        room.pid.push(ws.id);
+                        if (room.curPlayers === 2)
+                        {
+                            sendToClient(room.pid[0], "success");
+                            ws.send("success");
+                        }
+                    }
+                }
+                else {
+                    rooms.set(payload.ROOM_NAME, {
+                        curPlayers: 1,
+                        pid: [ws.id]
+                    });
+                }
+                break;
+            }
+            default:
+        }
+    })
+})
+
+server.listen(port, function() {
+    console.log("Listening");
+})
